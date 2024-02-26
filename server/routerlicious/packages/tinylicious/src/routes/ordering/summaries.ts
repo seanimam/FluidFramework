13b55d4f0231439675ea167b3d00ddafe19acae0
seanimam <105244057+seanimam@users.noreply.github.com>
2024-02-26 20:49:00 +0000

    adds types and refactors code further

--- server/routerlicious/packages/tinylicious/src/routes/ordering/summaries.ts
+++ server/routerlicious/packages/tinylicious/src/routes/ordering/summaries.ts
@@ -4,8 +4,8 @@
  */
 
 import { Router } from "express";
-import type { ITenantManager } from "@fluidframework/server-services-core";
-// import { SummaryReader } from "@fluidframework/server-lambdas";
+import type { ITenant, ITenantManager } from "@fluidframework/server-services-core";
+import type { ICommitDetails } from "@fluidframework/gitresources";
 import { getParam } from "../../utils";
 
 /**
@@ -15,16 +15,6 @@ import { getParam } from "../../utils";
 export function create(tenantManager: ITenantManager): Router {
 	const router: Router = Router();
 
-	addGetSummaryRoute(router, tenantManager);
-
-	return router;
-}
-
-/**
- * Adds API route to get the a summary for a given document, tenant id and git commit hash.
- * The git commit hash is effectively the UUID for a given summary.
- */
-function addGetSummaryRoute(router: Router, tenantManager: ITenantManager) {
 	router.get("/:tenantId/:documentId", (request, response) => {
 		console.log("received summary get request");
 		const documentId = getParam(request.params, "documentId");
@@ -34,62 +24,8 @@ function addGetSummaryRoute(router: Router, tenantManager: ITenantManager) {
 
 		tenantManagerP
 			.then(async (tenant) => {
-				const commits = await tenant.gitManager.getCommits(documentId, 1);
-				const treeHash = commits[0].commit?.tree?.sha;
-				console.log(" tenant.gitManager.getCommits(documentId, 1) returned:", commits);
-				console.log(`Document git tree hash is ${treeHash}`);
-
-				const fullTree = await tenant.gitManager.getTree(treeHash, true);
-				console.log("obtained full tree: ", fullTree);
-
-				const ddsData = {};
-				for (const item of fullTree.tree) {
-					if (item.path.startsWith(".channels") && item.type === "blob") {
-						if (item.path.endsWith(".component")) {
-							// Not sure what this data is supposed to be but it doesn't seem useful
-							continue;
-						}
-
-						// 1. Identify UUID for the DDS
-						let ddsCommitChannelId;
-						const pathParts = item.path.split("/");
-						if (pathParts.length > 3) {
-							// the channel id exists within item.path and I think its like the uuid name for a folder in git for a given dds
-							ddsCommitChannelId = pathParts[3];
-						} else {
-							console.error(
-								"Unable to identify which dds data belongs to due to missing channel id:",
-								item,
-							);
-						}
-						if (!ddsData[ddsCommitChannelId]) {
-							ddsData[ddsCommitChannelId] = {};
-						}
-
-						// 2. Decode git commit blob contents
-						const gitChannelData = await tenant.gitManager.getBlob(item.sha);
-						const decodedCommitContents = atob(gitChannelData.content);
-
-						// 3. Identify what type of information about the DDS this is
-						if (item.path.endsWith("header")) {
-							// This is the actual content in the DDS
-							if (ddsCommitChannelId) {
-								ddsData[ddsCommitChannelId].data = decodedCommitContents;
-							}
-						} else if (item.path.endsWith(".attributes")) {
-							// This is the metadata about the DDS
-							if (ddsCommitChannelId) {
-								ddsData[ddsCommitChannelId].metadata = decodedCommitContents;
-							}
-						}
-					}
-				}
-
-				const appData = { gitCommit: commits, ddsData };
-
-				return response.status(200).json({
-					appData,
-				});
+				const responseData = await getSummaryApiHandler(tenant, documentId);
+				return response.status(200).json(responseData);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -98,4 +34,126 @@ function addGetSummaryRoute(router: Router, tenantManager: ITenantManager) {
 	});
 
 	return router;
+}
+
+/**
+ * Typed returned by the getSummary route
+ */
+export interface GetSummaryApiResponse {
+	/**
+	 * Information about the commit in which the DDS data is from.
+	 * This is useful for seeing if the data is from a newly initialized (app) or a summary of a app and if so how new it is.
+	 *
+	 * @example
+	 * A newly created document will have the following in the field gitCommit.commit.message:
+	 * ```
+	 * "New document\n"
+	 * ```
+	 *
+	 * 	 * @example
+	 * A summary created from a document will have a string *similar* to the following in gitCommit.commit.message:
+	 * ```
+	 * "Summary @22:20\n"
+	 * ```
+	 *
+	 *
+	 */
+	gitCommit: ICommitDetails;
+	ddsData: {
+		/**
+		 * Serialized form of {@link IChannelAttributes} which contains metadata about the DDS such as what type of dds it is.
+		 *
+		 * @example
+		 * Here are a few examples of potential values of the attributes.type field for some DDS's.
+		 *```
+		 * - "https://graph.microsoft.com/types/counter"
+		 * - "https://graph.microsoft.com/types/map"
+		 * - "https://graph.microsoft.com/types/mergeTree"
+		 * - "https://graph.microsoft.com/types/directory"
+		 * ```
+		 */
+		attributes: string;
+		/**
+		 * Serialized json of what the data within the DDS actually is.
+		 */
+		content: string;
+	}[];
+}
+
+/**
+ * API Logic to get the summary of app data for a given document and tenant id
+ */
+async function getSummaryApiHandler(
+	tenant: ITenant,
+	documentId: string,
+): Promise<GetSummaryApiResponse> {
+	const commits = await tenant.gitManager.getCommits(documentId, 1);
+	const treeHash = commits[0].commit?.tree?.sha;
+	console.log(" tenant.gitManager.getCommits(documentId, 1) returned:", commits);
+	console.log(`Document git tree hash is ${treeHash}`);
+
+	const fullTree = await tenant.gitManager.getTree(treeHash, true);
+	console.log("obtained full tree: ", fullTree);
+
+	const ddsContentMap = new Map<
+		string,
+		{ content: string | undefined; attributes: string | undefined }
+	>();
+	for (const item of fullTree.tree) {
+		if (item.path.startsWith(".channels") && item.type === "blob") {
+			if (item.path.endsWith(".component")) {
+				// Not sure what this data is supposed to be but it doesn't seem useful
+				continue;
+			}
+
+			// 1. Identify UUID for the DDS
+			let ddsCommitChannelId;
+			const pathParts = item.path.split("/");
+			if (pathParts.length > 3) {
+				// the channel id exists within item.path and I think its like the uuid name for a folder in git for a given dds
+				ddsCommitChannelId = pathParts[3];
+			} else {
+				console.error(
+					"Unable to identify which dds data belongs to due to missing channel id:",
+					item,
+				);
+			}
+			if (!ddsContentMap.has(ddsCommitChannelId)) {
+				ddsContentMap.set(ddsCommitChannelId, {
+					content: undefined,
+					attributes: undefined,
+				});
+			}
+
+			// 2. Decode git commit blob contents
+			const gitChannelData = await tenant.gitManager.getBlob(item.sha);
+			const decodedCommitContents = atob(gitChannelData.content);
+
+			// 3. Identify what type of information about the DDS this is
+			if (item.path.endsWith("header")) {
+				// This is the actual content in the DDS
+				if (ddsCommitChannelId) {
+					ddsContentMap.get(ddsCommitChannelId).content = decodedCommitContents;
+				}
+			} else if (item.path.endsWith(".attributes")) {
+				// This is the metadata about the DDS
+				if (ddsCommitChannelId) {
+					ddsContentMap.get(ddsCommitChannelId).attributes = decodedCommitContents;
+				}
+			}
+		}
+	}
+
+	const apiResponse: GetSummaryApiResponse = {
+		gitCommit: commits[0],
+		ddsData: [],
+	};
+
+	for (const ddsContent of Object.values(ddsContentMap)) {
+		apiResponse.ddsData.push({
+			attributes: ddsContent.attributes,
+			content: ddsContent.content,
+		});
+	}
+	return apiResponse;
 }
